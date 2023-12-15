fa633ccdb449706f2c414282565fc3215a55cca9
Corrine.Tan <tenglunt@gmail.com>
2023-12-16 01:08:34 +1100

    add devtools

---
+++ mycoolapp/src/main/java/com/corrtan/sprintboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,19 @@
+package com.corrtan.sprintboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+}
